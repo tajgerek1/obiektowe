8d292136644308773db767b5ab9c1af65175fff3
tajgerek1 <m.nazarko.tp22@zsme.tarnow.pl>
2025-04-01 15:22:45 +0200

    Add files via upload

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    /// 
+
+    public class Myitem
+    {
+        public string Name { get; set; }
+
+        public string Sname { get; set; }
+
+        public string Pesel { get; set; }
+    }
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Myitem item = new Myitem();
+
+            item.Name = "tak";
+            item.Sname = "nie";
+            item.Pesel = "11111111111";
+
+            listView.Items.Add(new {m_nID = 1, m_strName = item.Name});
+        }
+    }
+}
