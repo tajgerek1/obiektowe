641b916f5c15c3eef5a902100bd74c2e20b01402
tajgerek1 <m.nazarko.tp22@zsme.tarnow.pl>
2025-05-19 22:35:33 +0200

    Update MainWindow.xaml.cs

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,47 +1,125 @@
-﻿using System.Text;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace WpfApp1
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    /// 
-
-    public class Myitem
-    {
-        public string Name { get; set; }
-
-        public string Sname { get; set; }
-
-        public string Pesel { get; set; }
-    }
     public partial class MainWindow : Window
     {
+        private ObservableCollection<Window1.Uczen> uczniowie = new ObservableCollection<Window1.Uczen>();
+
         public MainWindow()
         {
             InitializeComponent();
+            lvUczniowie.ItemsSource = uczniowie;
         }
 
         
-
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void MenuWczytaj_Click(object sender, RoutedEventArgs e)
         {
-            Myitem item = new Myitem();
+            var dlg = new OpenFileDialog
+            {
+                Filter = "CSV (,) |*.csv|CSV (;) |*.csv",
+                Title = "Wczytaj listę uczniów"
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                uczniowie.Clear();
+                string delimiter = dlg.FilterIndex == 1 ? "," : ";";
+                foreach (var line in File.ReadAllLines(dlg.FileName, Encoding.UTF8))
+                {
+                    var cols = line.Split(delimiter);
+                    var u = new Window1.Uczen
+                    {
+                        Pesel = cols.ElementAtOrDefault(0) ?? "",
+                        Imie = cols.ElementAtOrDefault(1) ?? "",
+                        DrugieImie = cols.ElementAtOrDefault(2) ?? "",
+                        Nazwisko = cols.ElementAtOrDefault(3) ?? ""
+                    };
+                   
+                    uczniowie.Add(u);
+                }
+            }
+        }
 
-            item.Name = "tak";
-            item.Sname = "nie";
-            item.Pesel = "11111111111";
+        
+        private void MenuZapisz_Click(object sender, RoutedEventArgs e)
+        {
+            var dlg = new SaveFileDialog
+            {
+                Filter = "CSV (,) |*.csv|CSV (;) |*.csv",
+                Title = "Zapisz listę uczniów"
+            };
+            if (dlg.ShowDialog() == true)
+            {
+                string delimiter = dlg.FilterIndex == 1 ? "," : ";";
+                using var writer = new StreamWriter(dlg.FileName, false, Encoding.UTF8);
+                foreach (var u in uczniowie)
+                {
+                    writer.WriteLine(
+                        $"{u.Pesel}{delimiter}" +
+                        $"{u.Imie}{delimiter}" +
+                        $"{u.DrugieImie}{delimiter}" +
+                        $"{u.Nazwisko}{delimiter}" +
+                        $"{u.DataUrodzenia:d}{delimiter}" +
+                        $"{u.Telefon}{delimiter}" +
+                        $"{u.Adres}{delimiter}" +
+                        $"{u.Miejscowosc}{delimiter}" +
+                        $"{u.KodPocztowy}");
+                }
+            }
+        }
 
-            listView.Items.Add(new {m_nID = 1, m_strName = item.Name});
+        
+        private void MenuDodaj_Click(object sender, RoutedEventArgs e)
+        {
+            var okno = new Window1();
+            if (okno.ShowDialog() == true)
+                uczniowie.Add(okno.NowyUczen);
+        }
+
+        
+        private void MenuUsun_Click(object sender, RoutedEventArgs e)
+        {
+            var zazn = lvUczniowie.SelectedItems;
+            var doUsun = new List<Window1.Uczen>();
+            foreach (Window1.Uczen u in zazn)
+                doUsun.Add(u);
+            foreach (var u in doUsun)
+                uczniowie.Remove(u);
+        }
+
+        
+        private void ContextMenuEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (lvUczniowie.SelectedItem is Window1.Uczen wybrany)
+            {
+                var okno = new Window1(wybrany);
+                if (okno.ShowDialog() == true)
+                {
+                    int idx = uczniowie.IndexOf(wybrany);
+                    uczniowie[idx] = okno.NowyUczen;
+                }
+            }
+        }
+
+        
+        private void ContextMenuDelete_Click(object sender, RoutedEventArgs e)
+        {
+            MenuUsun_Click(sender, e);
+        }
+
+        
+        private void MenuOProgramie_Click(object sender, RoutedEventArgs e)
+        {
+            var about = new AboutWindow();
+            about.Owner = this;
+            about.ShowDialog();
         }
     }
 }
